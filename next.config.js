640f061dc96e4a4c22e072628fe181fd07f20251
Rasel Shikdar <imrasel597@gmail.com>
2025-03-29 16:44:23 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,16 +3,9 @@ module.exports = {
   images: {
     domains: ['openweathermap.org'],
   },
-  async headers() {
-    return [
-      {
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS' },
-        ],
-      }
+  experimental: {
+    fontLoaders: [
+      { loader: '@next/font/google', options: { subsets: ['latin'] } }
     ]
   }
 }
