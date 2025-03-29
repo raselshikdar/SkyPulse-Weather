69d8f42edb24cf49ce79a17a4014ac119a09585a
Rasel Shikdar <imrasel597@gmail.com>
2025-03-29 15:46:26 +0600

    Update WeatherCard.js

--- components/WeatherCard.js
+++ components/WeatherCard.js
@@ -3,10 +3,14 @@ export default function WeatherCard({ weather, city }) {
     <div className="max-w-md mx-auto my-4 p-6 bg-white rounded-lg shadow">
       <h2 className="text-2xl font-bold mb-2">{city}</h2>
       <div className="flex items-center">
-        <img src={`http://openweathermap.org/img/wn/${weather.icon}@2x.png`} alt="Weather Icon" className="w-20" />
+        <img
+          src={`http://openweathermap.org/img/wn/${weather.icon}@2x.png`}
+          alt="Weather Icon"
+          className="w-20"
+        />
         <div className="ml-4">
           <p className="text-4xl font-bold">{weather.temp}°C</p>
-          <p className="capitalize">{weather.weather}</p>
+          <p className="capitalize">{weather.weather[0]?.description}</p> {/* Fixed */}
         </div>
       </div>
     </div>
