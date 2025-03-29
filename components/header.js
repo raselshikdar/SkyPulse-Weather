389386821669d2069084731b8f74360ffe716e41
Rasel Shikdar <imrasel597@gmail.com>
2025-03-29 15:45:26 +0600

    Update header.js

--- components/header.js
+++ components/header.js
@@ -2,7 +2,10 @@ export default function Header({ onGeolocation }) {
   return (
     <header className="flex justify-between items-center p-4 bg-blue-600 text-white">
       <h1 className="text-xl font-bold">SkyPulse Weather</h1>
-      <button onClick={onGeolocation} className="p-2 bg-blue-500 hover:bg-blue-700 rounded">
+      <button
+        onClick={onGeolocation}
+        className="p-2 bg-blue-500 hover:bg-blue-700 rounded"
+      >
         Use Current Location
       </button>
     </header>
